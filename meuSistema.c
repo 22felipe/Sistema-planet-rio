a374ae064540470c0a02c8e90c467813f5ca4a97
Felipe Gabriel <felipegmarques32fg@gmail.com>
2025-08-17 00:19:01 -0300

    implementação de luas e planetas

--- meuSistema.c
+++ meuSistema.c
@@ -8,7 +8,12 @@
 #include <GL/glut.h>  // no Windows é <GL/glut.h>
 
 // Variáveis globais de rotação
-static int year = 0, day = 0;
+static int year_sun = 0;
+static int year_planet1 = 0;
+static int year_planet2 = 0;
+static int year_moons = 0;
+static int day = 0;
+
 
 // Inicializa parâmetros de rendering
 void init(void){
@@ -22,32 +27,77 @@ void display(void){
     //glutWireSphere(1.0, 20, 16); os dois outros numeros são a quantidade de "poligonos", horinzontal e vertuical
     glPushMatrix();
         glColor3f(1.0, 1.0, 0.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year, 1.0, 0.0, 0.0);
-        glRotatef((GLfloat) day, 0.0, 0.0, 1.0);
+        glRotatef((GLfloat) year_sun, 0.0, 1.0, 0.0);
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
         glutWireSphere(1.0, 20, 16);   /* desenha o sol */
     glPopMatrix();
 
-    glPushMatrix();
-        glColor3f(1.0, 0.0, 0.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year, 0.0, 1.0, 0.0);
-        glTranslatef(2.0, 0.0, 0.0); //Translada após a rotação
-        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.2, 10, 8);   /* desenha o planeta */
-    glPopMatrix();
-
-
+    
     glPushMatrix();
         glColor3f(0.0, 0.0, 1.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year, 0.0, 1.0, 0.0);
+        glRotatef((GLfloat) year_planet2, 0.0, 1.0, 0.0);
         glTranslatef(4.0, 0.0, 0.0); //Translada após a rotação
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
         glutWireSphere(0.2, 10, 8);   /* desenha o planeta */
     glPopMatrix();
 
+    /*
+        para que as luas girem em volta do planeta precisamos colocala dentro
+        da matriz de transformação do planeta desejado
+    */
+    glPushMatrix();
+        glColor3f(1.0, 0.0, 0.0);//cor das linhas dos objetos
+        glRotatef((GLfloat) year_planet1, 0.0, 1.0, 0.0);
+        glTranslatef(2.0, 0.0, 0.0); //Translada após a rotação
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
+        glutWireSphere(0.2, 10, 8);   /* desenha o planeta */
+
+        glPushMatrix();
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
+        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
+        glTranslatef(0.5, 0.0, 0.0); //Translada após a rotação
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
+        glutWireSphere(0.1, 10, 8);   // desenha lua
+        glPopMatrix();  
+
+        glPushMatrix();
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
+        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
+        glTranslatef(-0.5, 0.0, 0.0); //Translada após a rotação
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
+        glutWireSphere(0.1, 10, 8);   // desenha lua
+        glPopMatrix();
+
+    glPopMatrix();
+
+
+    /*
+    glPushMatrix();
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
+        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
+        glTranslatef(2.5, 0.0, 0.0); //Translada após a rotação
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
+        glutWireSphere(0.1, 10, 8);   // desenha lua
+    glPopMatrix();  
+
+    glPushMatrix();
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
+        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
+        glTranslatef(1.5, 0.0, 0.0); //Translada após a rotação
+        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
+        glutWireSphere(0.1, 10, 8);   // desenha lua
+    glPopMatrix();
+    */
+
+
+    
+
     glutSwapBuffers(); // usado quando GLUT_DOUBLE está ativo
 }
 
-// Função de ajuste de viewport
+
+// Função de ajuste de viewport (visão de cima)
+/*
 void reshape(int w, int h){
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 
@@ -55,7 +105,25 @@ void reshape(int w, int h){
     glLoadIdentity();
 
     gluPerspective(60.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
-    gluLookAt(0.0, 0.0, 5.0,
+    
+    // Câmera de cima olhando para o sol
+    gluLookAt(0.0, 8.0, 0.0,   // posição da câmera (Y positivo = acima)
+              0.0, 0.0, 0.0,   // olhar para a origem
+              0.0, 0.0, -1.0); // define o eixo Z como "para cima" da tela
+
+    glMatrixMode(GL_MODELVIEW);
+}*/
+
+
+// Função de ajuste de viewport (visão frontal)
+void reshape(int w, int h){
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(60.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
+    gluLookAt(0.0, 0.0, 8.0,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
     glMatrixMode(GL_MODELVIEW);
@@ -73,11 +141,17 @@ void keyboard(unsigned char key, int x, int y){
             glutPostRedisplay();
             break;
         case 'y':
-            year = (year + 5) % 360;
+            year_sun = (year_sun + 5) % 360;
+            year_moons = (year_moons + 5) % 360;
+            year_planet1 = (year_planet1 + 5) % 360;
+            year_planet2 = (year_planet2 - 5) % 360;
             glutPostRedisplay();
             break;
         case 'Y':
-            year = (year - 5) % 360;
+            year_sun = (year_sun - 5) % 360;
+            year_moons = (year_moons - 5) % 360;
+            year_planet1 = (year_planet1 - 5) % 360;
+            year_planet2 = (year_planet2 + 5) % 360;
             glutPostRedisplay();
             break;
         case 'q':
