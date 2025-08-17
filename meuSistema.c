bbcc2ed59c6278ba58ad10829328d8b47ef7342a
Felipe Gabriel <felipegmarques32fg@gmail.com>
2025-08-17 11:24:28 -0300

    código funcional

--- meuSistema.c
+++ meuSistema.c
@@ -1,11 +1,4 @@
-//
-//  main.cpp
-//  TransformacaoPlanetas
-//
-//  Adaptado para Windows
-//
-
-#include <GL/glut.h>  // no Windows é <GL/glut.h>
+#include <GL/glut.h> 
 
 // Variáveis globais de rotação
 static int year_sun = 0;
@@ -14,90 +7,65 @@ static int year_planet2 = 0;
 static int year_moons = 0;
 static int day = 0;
 
-
 // Inicializa parâmetros de rendering
 void init(void){
     glClearColor(0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_DEPTH_TEST);
 }
 
 // Função callback chamada para fazer o desenho
 void display(void){
-    glClear(GL_COLOR_BUFFER_BIT);//cor de fundo
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//iniciando a cor de fundo e profundidade
     
-    //glutWireSphere(1.0, 20, 16); os dois outros numeros são a quantidade de "poligonos", horinzontal e vertuical
     glPushMatrix();
-        glColor3f(1.0, 1.0, 0.0);//cor das linhas dos objetos
+        glColor3f(1.0, 1.0, 0.0); //cor das linhas do objeto
         glRotatef((GLfloat) year_sun, 0.0, 1.0, 0.0);
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
         glutWireSphere(1.0, 20, 16);   /* desenha o sol */
     glPopMatrix();
 
-    
-    glPushMatrix();
-        glColor3f(0.0, 0.0, 1.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year_planet2, 0.0, 1.0, 0.0);
-        glTranslatef(4.0, 0.0, 0.0); //Translada após a rotação
-        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.2, 10, 8);   /* desenha o planeta */
-    glPopMatrix();
-
     /*
-        para que as luas girem em volta do planeta precisamos colocala dentro
+        para que as luas girem em volta do planeta precisamos coloca-las dentro
         da matriz de transformação do planeta desejado
     */
     glPushMatrix();
-        glColor3f(1.0, 0.0, 0.0);//cor das linhas dos objetos
+        glColor3f(1.0, 0.0, 0.0);//cor das linhas do objeto
         glRotatef((GLfloat) year_planet1, 0.0, 1.0, 0.0);
         glTranslatef(2.0, 0.0, 0.0); //Translada após a rotação
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.2, 10, 8);   /* desenha o planeta */
+        glutWireSphere(0.2, 10, 8);   /* desenha o planeta 1*/
 
         glPushMatrix();
-        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas do objeto
         glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
         glTranslatef(0.5, 0.0, 0.0); //Translada após a rotação
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.1, 10, 8);   // desenha lua
+        glutWireSphere(0.1, 10, 8);   // desenha lua 1
         glPopMatrix();  
 
         glPushMatrix();
-        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
+        glColor3f(1.0, 1.0, 1.0);//cor das linhas do objeto
+        glRotatef((GLfloat) year_moons, 1.0, 1.0, 0.0);
         glTranslatef(-0.5, 0.0, 0.0); //Translada após a rotação
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.1, 10, 8);   // desenha lua
+        glutWireSphere(0.1, 10, 8);   // desenha lua 2
         glPopMatrix();
 
     glPopMatrix();
 
-
-    /*
     glPushMatrix();
-        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
-        glTranslatef(2.5, 0.0, 0.0); //Translada após a rotação
+        glColor3f(0.0, 0.0, 1.0);//cor das linhas do objeto
+        glRotatef((GLfloat) year_planet2, 0.0, 1.0, 0.0);
+        glTranslatef(4.0, 0.0, 0.0); //Translada após a rotação
         glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.1, 10, 8);   // desenha lua
-    glPopMatrix();  
-
-    glPushMatrix();
-        glColor3f(1.0, 1.0, 1.0);//cor das linhas dos objetos
-        glRotatef((GLfloat) year_moons, 0.0, 1.0, 0.0);
-        glTranslatef(1.5, 0.0, 0.0); //Translada após a rotação
-        glRotatef((GLfloat) day, 0.0, 1.0, 0.0);
-        glutWireSphere(0.1, 10, 8);   // desenha lua
+        glutWireSphere(0.2, 10, 8);   /* desenha o planeta 2 */
     glPopMatrix();
-    */
-
-
-    
 
     glutSwapBuffers(); // usado quando GLUT_DOUBLE está ativo
 }
 
-
-// Função de ajuste de viewport (visão de cima)
 /*
+// Função de ajuste de viewport (visão de cima)
 void reshape(int w, int h){
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 
@@ -107,10 +75,9 @@ void reshape(int w, int h){
     gluPerspective(60.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
     
     // Câmera de cima olhando para o sol
-    gluLookAt(0.0, 8.0, 0.0,   // posição da câmera (Y positivo = acima)
-              0.0, 0.0, 0.0,   // olhar para a origem
-              0.0, 0.0, -1.0); // define o eixo Z como "para cima" da tela
-
+    gluLookAt(0.0, 8.0, 0.0,   // posição da câmera (x, y, z)
+              0.0, 0.0, 0.0,   // ponto para onde a câmera está olhando (alvo)
+              0.0, 0.0, -1.0); // vetor "up" (qual direção é considerada para cima)
     glMatrixMode(GL_MODELVIEW);
 }*/
 
@@ -123,9 +90,9 @@ void reshape(int w, int h){
     glLoadIdentity();
 
     gluPerspective(60.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
-    gluLookAt(0.0, 0.0, 8.0,
-              0.0, 0.0, 0.0,
-              0.0, 1.0, 0.0);
+    gluLookAt(0.0, 0.0, 8.0, // posição da câmera (x, y, z)
+              0.0, 0.0, 0.0, // ponto para onde a câmera está olhando (alvo)
+              0.0, 1.0, 0.0); // vetor "up" (qual direção é considerada para cima)
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -164,8 +131,9 @@ void keyboard(unsigned char key, int x, int y){
 
 // Função principal
 int main(int argc, char** argv){
+
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Rotacao de Planetas");
